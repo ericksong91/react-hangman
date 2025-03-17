7ca94dd63553a185bff95a161d4841b98e4265fc
Eric Song <ericksong91@gmail.com>
2025-03-17 15:04:09 -0400

    Template for words done

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,7 +1,21 @@
 function HangmanWord() {
-  return (
-    <div>HangmanWord</div>
-  )
+    const word = "test"
+    const guessedLetters = ["t"]
+
+    const wordArr = word.split("").map((letter, index) => {
+        return (
+            <span key={`letter-#${index}-letter`} className="letter-container mx-5 border-b-[.1em] border-black">
+                <span className={`${letter}-#${index} ${guessedLetters.includes(letter) ? "visible" : "invisible"}`}>
+                    {letter}
+                </span>
+            </span>)
+    });
+
+    return (
+        <div className="hang-word flex gap-[.25rem] text-9xl font-bold font-mono uppercase">
+            {wordArr}
+        </div>
+    );
 };
 
 export default HangmanWord;
